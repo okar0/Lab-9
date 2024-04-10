08d2b6cdce340b12ecbbd9e0a1cb5a6f8b33c1f5
okar0 <oskar.dolega@gmail.com>
2024-04-09 21:59:04 -0400

    3

--- Encoder.py
+++ Encoder.py
@@ -9,27 +9,30 @@ def encoder(password):
     return encoded_password
 
 def main():
-    print("""Menu
+
+    while True:
+        encoded_password = ""
+        print("""Menu
 -------------
 1. Encode
 2. Decode
 3. Quit
 """)
-    option = input("Please enter an option: ")
-    if option == "1":
-        password_input = input("Please enter your password to encode: ")
-        encoded_password = encoder(password_input)
-        print("Your password has been encoded and stored!")
+        option = input("Please enter an option: ")
+        if option == "1":
+            password_input = input("Please enter your password to encode: ")
+            encoded_password = encoder(password_input)
+            print("Your password has been encoded and stored!")
 
-    elif option == "2":
-        decoded_password = None  # decode(encoded_password)
-        print(f'The encoded password is {encoder(password_input)}, and the original password is {decoded_password}')
+        elif option == "2":
+            decoded_password = decode(encoded_password)
+            print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}')
 
-    elif option == '3':
-        break
+        elif option == '3':
+            break
 
-    else:
-        print("Invalid option, please try again!")
+        else:
+            print("Invalid option, please try again!")
 
 if __name__ == "__main__":
     main()
